a3eb9dde8162cdaa0a88f33f3c0727abeaea23cf
yinouet <52395173+yinouet@users.noreply.github.com>
2023-04-08 19:02:57 +0900

    Add files via upload

---
+++ keyball/config.h
@@ -0,0 +1,36 @@
+// Copyright 2023 yinouet (@yinouet)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+/* SPI & PMW3360 settings. */
+#define SPI_DRIVER SPID0
+#define SPI_SCK_PIN GP22
+#define SPI_MISO_PIN GP20
+#define SPI_MOSI_PIN GP23
+#define POINTING_DEVICE_CS_PIN GP21
+
+#define POINTING_DEVICE_INVERT_X
+#define POINTING_DEVICE_ROTATION_90
+
+// Split parameters
+// #define SOFT_SERIAL_PIN    GP1    
+#define SPLIT_HAND_MATRIX_GRID  GP27, GP9
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT       2500
---
+++ keyball/rules.mk
@@ -0,0 +1,6 @@
+# This file intentionally left blank
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = vendor
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pmw3360
