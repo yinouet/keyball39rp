9c2295da2ed0126749b3f0666c5e2f7fb5f25c6b
yinouet <52395173+yinouet@users.noreply.github.com>
2023-09-29 11:45:18 +0900

    add keyball39.h

--- qmk_firmware/keyboards/keyballrp/keyballrp.h
+++ qmk_firmware/keyboards/keyballrp/keyballrp.h
@@ -47,7 +47,7 @@ enum keyball_keycodes {
     DRAGSCROLL_MODE_TOGGLE,
 };
 
-#        define DPI_MOD POINTER_DEFAULT_DPI_FORWARD
+#        define DPI_MOD POINTER_DEFAULT_DPI_FORWARD // KB 0
 #        define DPI_RMOD POINTER_DEFAULT_DPI_REVERSE
 #        define S_D_MOD POINTER_SNIPING_DPI_FORWARD
 #        define S_D_RMOD POINTER_SNIPING_DPI_REVERSE
@@ -136,75 +136,75 @@ void keyball_set_pointer_dragscroll_enabled(bool enable);
 
 // clang-format off
 
-#define LAYOUT_right_ball( \
-    L00, L01, L02, L03, L04,              R04, R03, R02, R01, R00, \
-    L10, L11, L12, L13, L14,              R14, R13, R12, R11, R10, \
-    L20, L21, L22, L23, L24,              R24, R23, R22, R21, R20, \
-    L30, L31, L32, L33, L34, L35,    R35, R34,                R30  \
-    ) \
-    { \
-        {   L00,   L01,   L02,   L03,   L04, KC_NO }, \
-        {   L10,   L11,   L12,   L13,   L14, KC_NO }, \
-        {   L20,   L21,   L22,   L23,   L24, KC_NO }, \
-        {   L30,   L31,   L32,   L33,   L34,   L35 }, \
-        {   R00,   R01,   R02,   R03,   R04, KC_NO }, \
-        {   R10,   R11,   R12,   R13,   R14, KC_NO }, \
-        {   R20,   R21,   R22,   R23,   R24, KC_NO }, \
-        {   R30, KC_NO, KC_NO, KC_NO,   R34,   R35 }, \
-    }
+// #define LAYOUT_right_ball( \
+//     L00, L01, L02, L03, L04,              R04, R03, R02, R01, R00, \
+//     L10, L11, L12, L13, L14,              R14, R13, R12, R11, R10, \
+//     L20, L21, L22, L23, L24,              R24, R23, R22, R21, R20, \
+//     L30, L31, L32, L33, L34, L35,    R35, R34,                R30  \
+//     ) \
+//     { \
+//         {   L00,   L01,   L02,   L03,   L04, KC_NO }, \
+//         {   L10,   L11,   L12,   L13,   L14, KC_NO }, \
+//         {   L20,   L21,   L22,   L23,   L24, KC_NO }, \
+//         {   L30,   L31,   L32,   L33,   L34,   L35 }, \
+//         {   R00,   R01,   R02,   R03,   R04, KC_NO }, \
+//         {   R10,   R11,   R12,   R13,   R14, KC_NO }, \
+//         {   R20,   R21,   R22,   R23,   R24, KC_NO }, \
+//         {   R30, KC_NO, KC_NO, KC_NO,   R34,   R35 }, \
+//     }
 
-#define LAYOUT_left_ball( \
-    L00, L01, L02, L03, L04,              R04, R03, R02, R01, R00, \
-    L10, L11, L12, L13, L14,              R14, R13, R12, R11, R10, \
-    L20, L21, L22, L23, L24,              R24, R23, R22, R21, R20, \
-    L30,                L34, L35,    R35, R34, R33, R32, R31, R30  \
-    ) \
-    { \
-        {   L00,   L01,   L02,   L03,   L04, KC_NO }, \
-        {   L10,   L11,   L12,   L13,   L14, KC_NO }, \
-        {   L20,   L21,   L22,   L23,   L24, KC_NO }, \
-        {   L30, KC_NO, KC_NO, KC_NO,   L34,   L35 }, \
-        {   R00,   R01,   R02,   R03,   R04, KC_NO }, \
-        {   R10,   R11,   R12,   R13,   R14, KC_NO }, \
-        {   R20,   R21,   R22,   R23,   R24, KC_NO }, \
-        {   R30,   R31,   R32,   R33,   R34,   R35 }, \
-    }
+// #define LAYOUT_left_ball( \
+//     L00, L01, L02, L03, L04,              R04, R03, R02, R01, R00, \
+//     L10, L11, L12, L13, L14,              R14, R13, R12, R11, R10, \
+//     L20, L21, L22, L23, L24,              R24, R23, R22, R21, R20, \
+//     L30,                L34, L35,    R35, R34, R33, R32, R31, R30  \
+//     ) \
+//     { \
+//         {   L00,   L01,   L02,   L03,   L04, KC_NO }, \
+//         {   L10,   L11,   L12,   L13,   L14, KC_NO }, \
+//         {   L20,   L21,   L22,   L23,   L24, KC_NO }, \
+//         {   L30, KC_NO, KC_NO, KC_NO,   L34,   L35 }, \
+//         {   R00,   R01,   R02,   R03,   R04, KC_NO }, \
+//         {   R10,   R11,   R12,   R13,   R14, KC_NO }, \
+//         {   R20,   R21,   R22,   R23,   R24, KC_NO }, \
+//         {   R30,   R31,   R32,   R33,   R34,   R35 }, \
+//     }
 
-#define LAYOUT_dual_ball( \
-    L00, L01, L02, L03, L04,              R04, R03, R02, R01, R00, \
-    L10, L11, L12, L13, L14,              R14, R13, R12, R11, R10, \
-    L20, L21, L22, L23, L24,              R24, R23, R22, R21, R20, \
-    L30,                L34, L35,    R35, R34,                R30  \
-    ) \
-    { \
-        {   L00,   L01,   L02,   L03,   L04, KC_NO }, \
-        {   L10,   L11,   L12,   L13,   L14, KC_NO }, \
-        {   L20,   L21,   L22,   L23,   L24, KC_NO }, \
-        {   L30, KC_NO, KC_NO, KC_NO,   L34,   L35 }, \
-        {   R00,   R01,   R02,   R03,   R04, KC_NO }, \
-        {   R10,   R11,   R12,   R13,   R14, KC_NO }, \
-        {   R20,   R21,   R22,   R23,   R24, KC_NO }, \
-        {   R30, KC_NO, KC_NO, KC_NO,   R34,   R35 }, \
-    }
+// #define LAYOUT_dual_ball( \
+//     L00, L01, L02, L03, L04,              R04, R03, R02, R01, R00, \
+//     L10, L11, L12, L13, L14,              R14, R13, R12, R11, R10, \
+//     L20, L21, L22, L23, L24,              R24, R23, R22, R21, R20, \
+//     L30,                L34, L35,    R35, R34,                R30  \
+//     ) \
+//     { \
+//         {   L00,   L01,   L02,   L03,   L04, KC_NO }, \
+//         {   L10,   L11,   L12,   L13,   L14, KC_NO }, \
+//         {   L20,   L21,   L22,   L23,   L24, KC_NO }, \
+//         {   L30, KC_NO, KC_NO, KC_NO,   L34,   L35 }, \
+//         {   R00,   R01,   R02,   R03,   R04, KC_NO }, \
+//         {   R10,   R11,   R12,   R13,   R14, KC_NO }, \
+//         {   R20,   R21,   R22,   R23,   R24, KC_NO }, \
+//         {   R30, KC_NO, KC_NO, KC_NO,   R34,   R35 }, \
+//     }
 
-#define LAYOUT_no_ball( \
-    L00, L01, L02, L03, L04,              R04, R03, R02, R01, R00, \
-    L10, L11, L12, L13, L14,              R14, R13, R12, R11, R10, \
-    L20, L21, L22, L23, L24,              R24, R23, R22, R21, R20, \
-    L30, L31, L32, L33, L34, L35,    R35, R34, R33, R32, R31, R30  \
-    ) \
-    { \
-        {   L00,   L01,   L02,   L03,   L04, KC_NO }, \
-        {   L10,   L11,   L12,   L13,   L14, KC_NO }, \
-        {   L20,   L21,   L22,   L23,   L24, KC_NO }, \
-        {   L30,   L31,   L32,   L33,   L34,   L35 }, \
-        {   R00,   R01,   R02,   R03,   R04, KC_NO }, \
-        {   R10,   R11,   R12,   R13,   R14, KC_NO }, \
-        {   R20,   R21,   R22,   R23,   R24, KC_NO }, \
-        {   R30,   R31,   R32,   R33,   R34,   R35 }, \
-    }
+// #define LAYOUT_no_ball( \
+//     L00, L01, L02, L03, L04,              R04, R03, R02, R01, R00, \
+//     L10, L11, L12, L13, L14,              R14, R13, R12, R11, R10, \
+//     L20, L21, L22, L23, L24,              R24, R23, R22, R21, R20, \
+//     L30, L31, L32, L33, L34, L35,    R35, R34, R33, R32, R31, R30  \
+//     ) \
+//     { \
+//         {   L00,   L01,   L02,   L03,   L04, KC_NO }, \
+//         {   L10,   L11,   L12,   L13,   L14, KC_NO }, \
+//         {   L20,   L21,   L22,   L23,   L24, KC_NO }, \
+//         {   L30,   L31,   L32,   L33,   L34,   L35 }, \
+//         {   R00,   R01,   R02,   R03,   R04, KC_NO }, \
+//         {   R10,   R11,   R12,   R13,   R14, KC_NO }, \
+//         {   R20,   R21,   R22,   R23,   R24, KC_NO }, \
+//         {   R30,   R31,   R32,   R33,   R34,   R35 }, \
+//     }
 
-// clang-format on
+// // clang-format on
 
-#define LAYOUT LAYOUT_right_ball
-#define LAYOUT_universal LAYOUT_no_ball
+// #define LAYOUT LAYOUT_right_ball
+// #define LAYOUT_universal LAYOUT_no_ball
